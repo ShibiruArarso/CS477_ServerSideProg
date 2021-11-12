fcbb51a0b2ebd5e9c69fe7087492be1831ceb9eb
latnera <latnera2016@gmail.com>
2021-11-11 23:42:57 -0600

    read and write file

--- Read_and_write_File/index.js
+++ Read_and_write_File/index.js
@@ -1,15 +1,19 @@
 const fs = require("fs");
 
-// // Blocking, synchronous way
-// const rd = fs.readFileSync("introduce.txt", "utf-8");
-// console.log(rd);
-// const wr = `THIS IS THE TITLE OF SHIBIRU. \n ${rd}`;
-// fs.writeFileSync("write.txt", wr);
-// console.log(wr);
+//! Non-Bloking|asynchronous way Reading
+fs.readFile("./introduce.txt", "utf-8", (error, data) => {
+   console.log(data);
+});
 
-//Non-Bloking, asynchronous
-fs.readFile("introduce.txt", "utf-8", (e, d) => {
-   fs.writeFile("asyncWrite.txt", `${d}`, "utf-8", (e) => {
-      console.log("done writting");
+//! Non-Bloking|asynchronous way write
+fs.writeFile("./write.txt", `This is writing practice`, "utf-8", (error) => {
+   console.log("Done");
+});
+
+// //!Non-Bloking | asynchronous read and write at the same time
+let news = "What I read from file of introduce.txt is here plus additon comment";
+fs.readFile("./introduce.txt", "utf-8", (error, data) => {
+   fs.writeFile("./readWrite.txt", `${news} \n ${data}`, "utf-8", (error) => {
+      console.log("Done");
    });
 });
